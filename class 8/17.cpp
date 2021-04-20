1070feb0fd063744985e094604d5ef0882a484b6
Gabriel Agbese <gabriel.agbese2001@gmail.com>
2021-04-20 18:31:24 +0200

    Updated 17

--- class 8/17.cpp
+++ class 8/17.cpp
@@ -7,23 +7,8 @@
 int loop_int = 0;
 
 
-
-   
-
-void print_10_array_2 ( ostream& os, int* a ) {
-//1.5
-    cout << "1.5" <<endl;
-    for( loop_int = 0; loop_int < 10; loop_int++ ){
-		cout << (loop_int + 1) << " \t value: " << a [ loop_int ]; 
-        cout                           << endl;
-
-        
-    }
-    cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
-
-}
 	
-void print_10_array ( ostream& os, int* a ) {
+void print_array10( ostream& os, int* a ) {
  //1.4
     cout << "1.4" <<endl;
     
@@ -37,12 +22,9 @@ void print_10_array ( ostream& os, int* a ) {
 
 }
 
-
-
-void print_11_array ( ostream& os, int* a, int n ) {
+void print_array ( ostream& os, int* a, int n ) {
 
 //1.7
-    cout << "1.7" <<endl;
     for( loop_int = 0; loop_int < n; loop_int++ ){
 		cout << (loop_int + 1) << " \t value: " << a [ loop_int ]; 
         cout                           << endl;
@@ -53,29 +35,35 @@ void print_11_array ( ostream& os, int* a, int n ) {
 
 }
 
-void print_20_array ( ostream& os, int* a, int n ) {
+void print_vector ( ostream& os, vector<int*> b, int n ) {
 
-//1.8
-    cout << "1.7" <<endl;
-    
-    for( loop_int = 0; loop_int < n; loop_int++ ){
-		cout << (loop_int + 1) << " \t value: " << a [ loop_int ]; 
-        cout                                            << endl;
+    //1.7
+
+        for( loop_int = 0; loop_int < b.size(); loop_int++ ) {
+                cout << (loop_int + 1) << " \t value: " << *b [ loop_int ]; 
+                cout << endl;
+            }
+
+        for( loop_int = 0; loop_int < b.size(); loop_int++ ) {
+    	    delete b [ loop_int ];
+        }
     }
+    
 
-    cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
-}
+
+
+
 
 // allocation to array
 int* array_value_allocation ( int n ){
 
 	int* Array_Copy = new int [ n ];
 
-	int Initalizer = 100;
+	int init = 100;
 
     // storage in array
 	for( loop_int = 0; loop_int < n; loop_int++ ) {
-		Array_Copy [ loop_int ] = Initalizer++;
+		Array_Copy [ loop_int ] = init++;
     }
 
 	return Array_Copy;
@@ -86,15 +74,20 @@ vector <int*> vector_value_allocation ( int n ){
 
 	vector < int* > Vector_Copy;
 
-	int Initalizer = 100;
+	int init = 100;
 
     // storage in vector
 	for( loop_int = 0; loop_int < n; loop_int++ ) {
-		Vector_Copy.push_back( new int { Initalizer++ } );
+		Vector_Copy.push_back( new int { init++ } );
     }
 
 	return Vector_Copy;
 }
+
+
+
+
+
 
 
 int main(){
@@ -116,7 +109,7 @@ int main(){
 
     //1.4
     cout << "1.4" <<endl;
-    print_10_array ( cout, ten_int_array);
+    print_array10( cout, ten_int_array);
 	    delete[] ten_int_array;
     cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
     
@@ -124,35 +117,23 @@ int main(){
     int* ten_int_array_2 = array_value_allocation ( 10 );
 
     cout << "1.5" <<endl;
-    print_10_array_2 ( cout, ten_int_array_2 );
+    print_array ( cout, ten_int_array_2, 10);
     cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
 	delete[] ten_int_array_2;
 
+
     //1.6
-
     int* eleven_int_array = array_value_allocation ( 11 );
+
     cout << "1.6" <<endl;
-
-    for( loop_int = 0; loop_int < 11; loop_int++ ) {
-		    cout << (loop_int + 1) << " \t value: " << eleven_int_array [ loop_int ]; 
-            cout  << endl;
-        }
-        cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
-
+    print_array ( cout, eleven_int_array, 11);
     delete[] eleven_int_array;
-
-    //1.7
-    int* eleven_int_array_2 = array_value_allocation ( 11 );
-
-    cout << "1.7" <<endl;
-    print_11_array ( cout, eleven_int_array, 11);
-    delete[] eleven_int_array_2;
     cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
 
     //1.8
     cout << "1.8" <<endl;
     int* twenty_int_array = array_value_allocation ( 20 );
-    print_20_array ( cout, twenty_int_array, 20 );
+    print_array ( cout, twenty_int_array, 20 );
     cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
 
     //1.9
@@ -161,47 +142,29 @@ int main(){
     //1.10
     //VECTOR VERSIONS
 
+
     //1.5(vector)
     vector <int*> ten_int_vector = vector_value_allocation ( 10 );
     cout << "1.10.5" <<endl;
-    for( loop_int = 0; loop_int < ten_int_vector.size(); loop_int++ ){
-		    cout << (loop_int + 1) << " \t value: " << *ten_int_vector [ loop_int ]; 
-            cout                                  << endl;
-        }
-        cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
-
-    for( loop_int = 0; loop_int < ten_int_vector.size(); loop_int++ ) {
-    	    delete ten_int_vector [ loop_int ];
-        }
+    print_vector ( cout, ten_int_vector, 10 );
+    cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
 
     //1.6(vector)
     vector <int*> eleven_int_vector = vector_value_allocation ( 11 );
     cout << "1.10.6" <<endl;
-    for( loop_int = 0; loop_int < eleven_int_vector.size(); loop_int++ ) {
-		    cout << (loop_int + 1) << " \t value: " << *eleven_int_vector [ loop_int ]; 
-            cout << endl;
-        }
+    print_vector ( cout, eleven_int_vector, 11 );
     cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
-    
-    for( loop_int = 0; loop_int < eleven_int_vector.size(); loop_int++ ) {
-    	    delete eleven_int_vector [ loop_int ];
-        }
     
     //1.8(vector)
 
     vector < int* > twenty_int_vector = vector_value_allocation ( 20 );
     
-    cout << "1.10.6" <<endl;
-    for( loop_int = 0; loop_int < twenty_int_vector.size(); loop_int++ ) {
-		    cout << (loop_int + 1) << " \t value: " << *twenty_int_vector [ loop_int ]; 
-            cout << endl;
-        }
+    cout << "1.10.8" <<endl;
+    print_vector ( cout, twenty_int_vector, 20 );
     cout << endl << "-----------------------------------------------------------------------------------" << endl << endl;
 
-    for( loop_int = 0; loop_int < twenty_int_vector.size(); loop_int++ ) {
-    	    delete twenty_int_vector [ loop_int ];
-        }
-    
+
+
     return 0;
 
 }
